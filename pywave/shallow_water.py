3cfd412a61dc17448b8238fceaafcc2827b7c29f
tdcwilliams <tdcwilliams@gmail.com>
2021-10-11 14:43:20 +0200

    New class ShallowWater which inherits from ElasticString

---
+++ pywave/shallow_water.py
@@ -0,0 +1,6 @@
+from pywave.elastic_string import ElasticString
+
+
+class ShallowWater(ElasticString):
+    def __init__(self, rho_water=1025, depth=100, period=20, gravity=9.81, xlim=None):
+        super().__init__(m=1, kappa = depth*gravity, period=period, xlim=xlim)
