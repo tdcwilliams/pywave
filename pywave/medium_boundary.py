de3dd0439b1b85352d5798d7bb9973c86c5d5d6c
tdcwilliams <tdcwilliams@gmail.com>
2021-11-04 15:08:22 +0100

    MediumBoundary has solve method now

--- pywave/medium_boundary.py
+++ pywave/medium_boundary.py
@@ -14,10 +14,10 @@ class MediumBoundary(ScattererBase):
         
         Parameters:
         -----------
-        lhs : ElasticString or subclass
-            object containing properties of the string segment to the left of the boundary
-        rhs : ElasticString or subclass
-            object containing properties of the string segment to the right of the boundary
+        lhs : Medium or subclass
+            object containing properties of the medium segment to the left of the boundary
+        rhs : Medium or subclass
+            object containing properties of the medium segment to the right of the boundary
         position : float
             x value where the boundary occurs
         """
@@ -28,7 +28,7 @@ class MediumBoundary(ScattererBase):
             ])
 
 
-    def solve(self):
+    def solve(self, condition_types=None):
         """
         Solve the boundary conditions to set the scattering matrices
         
@@ -43,8 +43,46 @@ class MediumBoundary(ScattererBase):
         Tm_ : numpy.ndarray(float)
             T^- matrix (transmission of waves to left)
         """
-        raise NotImplementedError(
-                "solve method needs to be implemented in child classes")
+        forcings = []
+        matrices = []
+        for n, med in enumerate(self.media):
+            # sort the operators
+            med_factor = -1 ** n
+            edge_ops = []
+            for (op1, op2), is_continuous in zip(
+                    med.edge_operators, condition_types):
+                edge_ops += [med_factor * op1]
+                if is_continuous:
+                    edge_ops += [med_factor * op2]
+
+            """
+            1st half of matrix columns: "e^{ikx}" eigen functions
+            2nd half of matrix columns: "e^{-ikx}" eigen functions
+            """
+            nk = len(med.k)
+            matrix = np.zeros((len(edge_ops), 2*nk)
+            for i, op in enumerate(edge_ops):
+                for j, k in enumerate(med.k):
+                matrix[i, j]    = op(k)
+                matrix[i, j+nk] = op(-k)
+            """
+            for LHS (n=0):
+                want the incident waves to be from left to right so choose
+                "+k" columns from matrix
+            for RHS (n=1):
+                want the incident waves to be from right to left so choose
+                "-k" columns from matrix
+            """
+            forcings += [matrix[:,n*nk:(n+1)*nk]]
+
+        unknowns = np.linalg.solve(
+            np.hstack(matrices), np.hstack(forcings))
+        nk0 = len(self.media[0].k)
+        nk1 = len(self.media[1].k)
+        self.Rp_ = unknowns[:nk0, :nk0]            #reflected to left
+        self.Tm_ = unknowns[nk0:2*nk0, nk0:]       #transmitted from right
+        self.Tm_ = unknowns[2*nk0:2*nk0+nk1, :nk0] #transmitted from left
+        self.Rm_ = unknowns[2*nk0+nk1:, nk0:]      #reflected from right
 
 
     def test_boundary_conditions(self, inc_amps=None):
