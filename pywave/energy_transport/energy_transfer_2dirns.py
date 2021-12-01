5148a653f604d3a2a11cef162172fe13668f91f7
tdcwilliams <tdcwilliams@gmail.com>
2021-12-01 10:52:12 +0100

    rejig energy_transfer_2dirns.py to use lib.py for easier testing

--- pywave/energy_transport/energy_transfer_2dirns.py
+++ pywave/energy_transport/energy_transfer_2dirns.py
@@ -1,7 +1,6 @@
 import numpy as np
 
-from pywave.energy_transport.lib import (
-        diffl, diffr, sumr)
+from pywave.energy_transport.lib import solve_2d_ode_spectral
 from pywave.energy_transport.advect1d import Advect1D
 
 
@@ -74,9 +73,9 @@ class EnergyTransfer2Dirns(Advect1D):
             element (1,1) of source matrix
         """
         # make alpha and gamma be arrays of the right shape
-        alp = (alpha if isinstance(alpha, numpy.ndarray)
+        alp = (alpha if isinstance(alpha, np.ndarray)
                 else np.full(shape, alpha))
-        gam = (gamma if isinstance(gamma, numpy.ndarray)
+        gam = (gamma if isinstance(gamma, np.ndarray)
                 else np.full(shape, gamma))
 
         # set the source matrix [[a,b],[c,d]]
@@ -118,15 +117,22 @@ class EnergyTransfer2Dirns(Advect1D):
         v_ = self.advect(v, -c)
         # set the source matrix [[a,b],[c,d]]
         a, b, c, d = self.get_source_matrix(alpha, gamma, u.shape)
-        # convert to the matrix to invert
-        # u^{n+1}, v^{n+1} = ...
-        #     + (a*u^{n+1} + b*v^{n+1}, c*u^{n+1} + d*v^{n+1})
-        a, b, c, d = 1-a, -b, -c, 1-d
-        det = a*d - b*c
-        return (d*u_ - b*v_)/det, (-c*u_ + a*v_)/det
+        """
+        system to solve:
+        u^{n+1}, v^{n+1} = ...
+            + (a*u^{n+1} + b*v^{n+1}, c*u^{n+1} + d*v^{n+1})
+        ie need to invert
+            A = [[1-a,-b],[-c,1-d]]
+        This has inverse
+            Ainv = [[1-d,b],[c,1-a]]/det
+        where
+            det = (1-d)*(1-a) - b*c
+        """
+        det = (1-a)*(1-d) - b*c
+        return ((1-d)*u_ + b*v_)/det, (c*u_ + (1-a)*v_)/det
 
 
-    def step_explicit(self, u, c, alpha, gamma):
+    def step_explicit(self, u, v, c, alpha, gamma):
         """
         Advect and attenuate u for one time step.
         Do attenuation explicitly.
@@ -151,155 +157,6 @@ class EnergyTransfer2Dirns(Advect1D):
                 self.advect(u,  c) + a*u + b*v,
                 self.advect(v, -c) + c*u + d*v,
                 )
-
-
-    def evolve_unique_evals(self, u, v, lams, ab):
-        """
-        Evolve ODE
-        [du/dt, dv/dt] = [[a,b],[c,d]](u,v) 
-        from time 0 to self.dt
-        when the eigenvalues of the matrix are not repeated.
-        Note that in this case we don't need to know the 2nd
-        row of the matrix (ie just need (a,b)).
-
-        Parameters:
-        -----------
-        u : numpy.ndarray
-            quantity to be advected to right
-        v : numpy.ndarray
-            quantity to be advected to left
-        lams : list(numpy.ndarray)
-            list of eigenvalues [lam1,lam2]
-        ab : tuple
-            (a,b) with
-            a: numpy.ndarray
-                element (0,0) of source matrix
-            b: numpy.ndarray
-                element (0,1) of source matrix
-
-        Returns:
-        --------
-        u_new : numpy.ndarray
-            updated quantity to be advected to right
-        v_new : numpy.ndarray
-            updated quantity to be advected to left
-        """
-        a, b = ab
-        u_new = np.zeros_like(u)
-        v_new = np.zeros_like(u)
-        for lam in lams:
-            h = np.hypot(-b, a-lam)
-            ev = np.array([[-b/h], [a-lam]/h]) #2 x nx
-            coef = u*ev[0] + v*ev[1]
-            ex = np.exp(lam*self.dt)
-            u_new += ex * coef * ev[0]
-            v_new += ex * coef * ev[1]
-        return u_new, v_new
-
-
-    def evolve_repeated_eval(self, u, v, lam, abcd):
-        """
-        Evolve ODE
-        [du/dt, dv/dt] = [[a,b],[c,d]](u,v) 
-        from time 0 to self.dt
-        when the eigenvalues of the matrix are repeated
-
-        Parameters:
-        -----------
-        u : numpy.ndarray
-            quantity to be advected to right
-        v : numpy.ndarray
-            quantity to be advected to left
-        lam : numpy.ndarray
-            eigenvalue
-        abcd : tuple
-            (a,b,c,d) with
-            a: numpy.ndarray
-                element (0,0) of source matrix
-            b: numpy.ndarray
-                element (0,1) of source matrix
-            c: numpy.ndarray
-                element (1,0) of source matrix
-            d: numpy.ndarray
-                element (1,1) of source matrix
-
-        Returns:
-        --------
-        u_new : numpy.ndarray
-            updated quantity to be advected to right
-        v_new : numpy.ndarray
-            updated quantity to be advected to left
-        """
-
-        """
-        eigenvector satisfies
-        (a-lam)*x+b*y=0
-        ie orthogonal to row=[a-lam,b]/h
-        (row is normalised to unit vector)
-        """
-        a, b, c, d = abcd
-        rows = {} # key True: use top row of matrix to find aux
-                  # key False: use bottom row
-        for i, (el0, el1) in enumerate([
-                (a-lam, b), #2 x nx
-                (c, d-lam), #2 x nx
-                ]):
-            h = hypot(el0, el1)
-            rows[i==0] = [el0/h, el1/h, h]
-
-        ev0 = -el1/h
-        ev1 = el0/h
-        rows[True] += [ev0]
-        rows[False] += [ev1]
-        ex = np.exp(lam*self.dt)
-        tmp = ex * (u*ev0 + v*ev1)
-        u_new = tmp * ev0
-        v_new = tmp * ev1
-
-        """
-        uv2 = exp(lam*t)*(ev*t + aux)
-        WHERE IF ev0 != 0:
-        auxiliary vector aux=(x,y) satisfies
-        (a-lam)*x+b*y=ev0
-        or
-        h*row.dot(aux) = ev0
-        with row=(a-lam,b)/h a unit vector.
-
-        IF ev0 ==0, THEN ev1 != 0:
-        auxiliary vector aux=(x,y) satisfies
-        c*x+(d-lam)*y=ev1
-        or
-        h*row.dot(aux) = ev1
-        with row=(c,d-lam)/h a unit vector.
-
-        IN GENERAL:
-        h*row.dot(aux) = ev_
-        with row=(r0,r1) a unit vector.
-
-        eg aux = (ev_/h)*row +beta*(ev0,ev1)
-        Choose beta=0 so aux is orthogonal to (ev0,ev1).
-        Then
-        h*row.dot(aux) = h*row.dot((ev_/h)*row) = ev_
-        => aux = (ev_/h)*row
-        => uv2 = (ev_/h)*exp(lam*t)*((h/ev_)*ev*t + row)
-        or just
-        uv2 = exp(lam*t)*((h/ev_)*ev*t + row) so its coefficient
-        is just (u,v).dot(row) since row is a unit vector
-        """
-        def get_update(b, use):
-            r0, r1, h, ev_ = rows[b]
-            # h*(r0,r1).dot(aux) = ev_
-            tmp = ex[use] * (u[use]*r0[use] + v[use]*r1[use])
-            tmp2 = (h*self.dt/ev_)[use]
-            du = tmp*(tmp2*ev0[use] + r0[use])
-            dv = tmp*(tmp2*ev1[use] + r1[use])
-            return du, dv
-        for i, use in enumerate((ev0 != 0, ev==0)):
-            if np.any(use):
-                du, dv = get_update(i==0, use)
-                u_new[use] += du
-                v_new[use] += dv
-        return u_new, v_new
 
 
     def step_split(self, u, v, c, alpha, gamma):
@@ -332,26 +189,6 @@ class EnergyTransfer2Dirns(Advect1D):
 
         # set the source matrix [[a,b],[c,d]]
         a, b, c, d = self.get_source_matrix(alpha, gamma, u.shape)
-        discr = (a+d)**2 + a*d-b*c
-        assert(np.all(discr >= 0))
-        lam_av = (a+d)/2 # average of the 2 eigenvalues
-        u_new = np.copy(u_)
-        v_new = np.copy(v_)
-
-        # 1st do repeated eigenvalues (both = lam_av)
-        use = (discr == 0)
-        if np.any(use):
-            abcd = (a[use], b[use], c[use], d[use])
-            u_new[use], v_new[use] = self.evolve_repeated_eval(
-                    u_[use], v_[use], lam_av[use], abcd)
-
-        # 2nd do unique eigenvalues
-        use = (discr > 0)
-        if np.any(use):
-            ab = (a[use], b[use])
-            dlam = np.sqrt(discr[use])/2
-            lams = [lam_av[use] + dlam, lam_av[use]-dlam]
-            u_new[use], v_new[use] = self.evolve_unique_evals(
-                    u_[use], v_[use], lams, ab)
-
-        return u_new, v_new
+        u_new, v_new = solve_2d_ode_spectral(
+                u_, v_, np.array([self.dt]), a, b, c, d)
+        return u_new[0], v_new[0]
